02f114840fc21400317ad8fa09dc2164ce04e1a8
adriansyahysf <bimogamerz77@gmail.com>
2024-05-30 18:11:00 +0700

    pw

---
+++ vue-project-praktikum8/src/components/ArticleHome.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <img alt="rover" class="rover" src="@/assets/rover.png" width="300" height="400" />
+    <h1>ini adalah article view</h1>
+    <p>ini adalah sisi gelap</p>
+    <p>ini adalah sisi gelap</p>
+    <p>ini adalah sisi gelap</p>
+  </div>
+</template>
---
+++ vue-project-praktikum8/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import ArticleHome from '../components/ArticleHome.vue'
+</script>
+
+<template>
+  <main>
+    <ArticleHome />
+  </main>
+</template>
